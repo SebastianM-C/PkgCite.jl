f7fe94b928455dd26c313d4a41401e4a57ee2be6
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-05-31 20:40:42 +0300

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using Cite
 using Test
+using Pkg
 
 @testset "Cite.jl" begin
-    cd("test_env")
+    Pkg.activate("test_env")
+
     citations = get_citations()
     @test haskey(citations, "DifferentialEquations.jl-2017")
     @test haskey(citations, "AbstractAlgebra.jl-2017")
