81c6ed305df3ee8201e9ca402f1fb8642015fe94
Documenter.jl <documenter@juliadocs.github.io>
2021-05-31 17:44:23 +0000

    build based on f7fe94b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Cite","category":"page"},{"location":"#Cite","page":"Home","title":"Cite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Cite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Cite]","category":"page"}]
+}
