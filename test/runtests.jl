740536cca7bc94b6c5fec6f139aef38d3217410e
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-06-06 16:29:38 +0300

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Cite
-using Cite: collect_citations, get_tool_citation
+using Cite: collect_citations, bibliography, cited_packages
+using Bibliography
 using Test
 using Pkg
 
@@ -11,13 +12,36 @@ using Pkg
 
     citations = collect_citations()
     @show citations
-    @test haskey(citations, "DifferentialEquations.jl-2017")
-    @test haskey(citations, "AbstractAlgebra.jl-2017")
-    @test haskey(citations, "quadgk")
-    @test haskey(citations, "gowda2021high")
+    @testset "Packages" begin
+        pkgs = cited_packages(citations)
+        @test "LabelledArrays" ∈ pkgs
+        @test "QuadGK" ∈ pkgs
+        @test "Symbolics" ∈ pkgs
+        @test "RecursiveArrayTools" ∈ pkgs
+        @test "ArrayInterface" ∈ pkgs
+        @test "AbstractAlgebra" ∈ pkgs
+        # There is an error in the CITATION.bib for Distributions
+        @test_broken haskey(citations, "Distributions")
+    end
+    @testset "Citations" begin
+        bib = bibliography(citations)
+        @test haskey(bib, "DifferentialEquations.jl-2017")
+        @test haskey(bib, "quadgk")
+        @test haskey(bib, "AbstractAlgebra.jl-2017")
+        @test haskey(bib, "gowda2021high")
+    end
 
     get_citations()
-    @test isfile(joinpath(@__DIR__, "julia_citations.bib"))
+    @testset "Citations in .bib" begin
+        bib_path = joinpath(@__DIR__, "julia_citations.bib")
+        @test isfile(bib_path)
+        bib = import_bibtex(bib_path)
+
+        @test haskey(bib, "DifferentialEquations.jl-2017")
+        @test haskey(bib, "quadgk")
+        @test haskey(bib, "AbstractAlgebra.jl-2017")
+        @test haskey(bib, "gowda2021high")
+    end
 
     get_tool_citation()
 end
