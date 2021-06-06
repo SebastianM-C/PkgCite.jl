19dfb3631d3463b97ef44085dd34977b7f97c5c7
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-06-06 17:04:45 +0300

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,10 @@ using Test
 using Pkg
 
 @testset "Cite.jl" begin
+    @testset "Empty env" begin
+        @test_warn "No citations found in current environment" get_citations()
+    end
+
     test_env = "test_env"
     Pkg.activate(test_env)
     Pkg.instantiate()
@@ -43,5 +47,32 @@ using Pkg
         @test haskey(bib, "gowda2021high")
     end
 
-    get_tool_citation()
+    @testset "Cite sentence" begin
+        io = IOBuffer()
+        get_tool_citation(io)
+        seekstart(io)
+        str = read(io, String)
+
+        @test str == "This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
+        "and made use of the following packages: "*
+        "LabelledArrays.jl\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
+        "QuadGK.jl\\cite[QuadGK]{quadgk}, Symbolics.jl\\cite[Symbolics]{gowda2021high}, "*
+        "RecursiveArrayTools.jl\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
+        "ArrayInterface.jl\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
+        "AbstractAlgebra.jl\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}.\n"
+
+        io = IOBuffer()
+        get_tool_citation(io, jl=false)
+        seekstart(io)
+        str = read(io, String)
+
+        @test str == "This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
+        "and made use of the following packages: "*
+        "LabelledArrays\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
+        "QuadGK\\cite[QuadGK]{quadgk}, Symbolics\\cite[Symbolics]{gowda2021high}, "*
+        "RecursiveArrayTools\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
+        "ArrayInterface\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
+        "AbstractAlgebra\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}.\n"
+    end
+
 end
