f2ce3e0d2a6972e24cebb872a3a3971b7d861ca3
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-06-07 03:01:04 +0300

    Move test sentences to separate file

---
+++ test/cite_str.jl
@@ -0,0 +1,27 @@
+const CITE_STR_JL =
+"This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
+"and made use of the following packages: "*
+"LabelledArrays.jl\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
+"QuadGK.jl\\cite[QuadGK]{quadgk}, Symbolics.jl\\cite[Symbolics]{gowda2021high}, "*
+"RecursiveArrayTools.jl\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
+"ArrayInterface.jl\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
+"AbstractAlgebra.jl\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}."
+
+const CITE_STR_JL_AUTO =
+"This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
+"and made use of the following packages: "*
+"LabelledArrays.jl\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
+"QuadGK.jl\\cite[QuadGK]{quadgk}, Symbolics.jl\\cite[Symbolics]{gowda2021high}, "*
+"RecursiveArrayTools.jl\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
+"ArrayInterface.jl\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
+"AbstractAlgebra.jl\\autocite[AbstractAlgebra]{AbstractAlgebra.jl-2017}."
+
+
+const CITE_STR =
+"This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
+"and made use of the following packages: "*
+"LabelledArrays\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
+"QuadGK\\cite[QuadGK]{quadgk}, Symbolics\\cite[Symbolics]{gowda2021high}, "*
+"RecursiveArrayTools\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
+"ArrayInterface\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
+"AbstractAlgebra\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}."
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Bibliography
 using Test
 import Pkg
 
+include("cite_str.jl")
+
 @testset "Cite.jl" begin
     @testset "Empty env" begin
         @test_logs (:warn, "No citations found in current environment") get_citations()
@@ -15,7 +17,7 @@ import Pkg
     Pkg.status()
 
     citations = collect_citations(false)
-    @show citations
+
     @testset "Packages" begin
         pkgs = cited_packages(citations)
         @test "LabelledArrays" ∈ pkgs
@@ -27,6 +29,7 @@ import Pkg
         # There is an error in the CITATION.bib for Distributions
         @test_broken haskey(citations, "Distributions")
     end
+
     @testset "Citations" begin
         bib = bibliography(citations)
         @test haskey(bib, "DifferentialEquations.jl-2017")
@@ -36,6 +39,7 @@ import Pkg
     end
 
     get_citations()
+
     @testset "Citations in .bib" begin
         bib_path = joinpath(@__DIR__, "julia_citations.bib")
         @test isfile(bib_path)
@@ -52,24 +56,17 @@ import Pkg
         pkgs = cited_packages(citations)
         @test only(pkgs) == "Symbolics"
     end
+
     @testset "Cite sentence" begin
+        citations = collect_citations(false)
         str = make_sentence(citations, cite_commands=Dict{String,String}())
-        @test str == "This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
-            "and made use of the following packages: "*
-            "LabelledArrays.jl\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
-            "QuadGK.jl\\cite[QuadGK]{quadgk}, Symbolics.jl\\cite[Symbolics]{gowda2021high}, "*
-            "RecursiveArrayTools.jl\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
-            "ArrayInterface.jl\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
-            "AbstractAlgebra.jl\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}."
+        @test str == CITE_STR_JL
 
         str = make_sentence(citations, cite_commands=Dict{String,String}(), jl=false)
-        @test str == "This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
-            "and made use of the following packages: "*
-            "LabelledArrays\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
-            "QuadGK\\cite[QuadGK]{quadgk}, Symbolics\\cite[Symbolics]{gowda2021high}, "*
-            "RecursiveArrayTools\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
-            "ArrayInterface\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
-            "AbstractAlgebra\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}."
+        @test str == CITE_STR
+
+        str = make_sentence(citations, cite_commands=Dict{String,String}("AbstractAlgebra"=>"\\autocite"), jl=false)
+        @test str == CITE_STR_JL_AUTO
 
         @testset "Clipboard" begin
             io = IOBuffer()
@@ -77,26 +74,14 @@ import Pkg
             seekstart(io)
             str = read(io, String)
 
-            @test_broken str == "This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
-            "and made use of the following packages: "*
-            "LabelledArrays.jl\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
-            "QuadGK.jl\\cite[QuadGK]{quadgk}, Symbolics.jl\\cite[Symbolics]{gowda2021high}, "*
-            "RecursiveArrayTools.jl\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
-            "ArrayInterface.jl\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
-            "AbstractAlgebra.jl\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}.\n"
+            @test_broken str == CITE_STR_JL*'\n'
 
             io = IOBuffer()
             get_tool_citation(io, jl=false)
             seekstart(io)
             str = read(io, String)
 
-            @test_broken str == "This work was done in \\cite[Julia v$VERSION]{Julia-2017} "*
-            "and made use of the following packages: "*
-            "LabelledArrays\\cite[LabelledArrays]{DifferentialEquations.jl-2017}, "*
-            "QuadGK\\cite[QuadGK]{quadgk}, Symbolics\\cite[Symbolics]{gowda2021high}, "*
-            "RecursiveArrayTools\\cite[RecursiveArrayTools]{DifferentialEquations.jl-2017}, "*
-            "ArrayInterface\\cite[ArrayInterface]{DifferentialEquations.jl-2017} and "*
-            "AbstractAlgebra\\cite[AbstractAlgebra]{AbstractAlgebra.jl-2017}.\n"
+            @test_broken str == CITE_STR * '\n'
         end
     end
 end
